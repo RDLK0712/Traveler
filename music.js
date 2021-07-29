0cbdf66f4cf04d2023fdc880d6895d7fbccd7a97
RDLK0712 <zxcnmijklpoq@gmail.com>
2021-07-29 23:14:21 +0800

    pixel

--- music.js
+++ music.js
@@ -1,14 +1,3 @@
-    function changeFrameHeight(){
-        var ifm= document.getElementById("iframepage"); 
-        ifm.height=document.documentElement.clientHeight;
-
-    }
-
-    window.onresize=function(){  
-         changeFrameHeight();  
-
-    } 
-
 $('.one-time').slick({
     dots: true,
     infinite: true,
