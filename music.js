0a81c86fdf13f74c4a3fef0616186fd23957f167
RDLK0712 <zxcnmijklpoq@gmail.com>
2021-07-29 22:22:57 +0800

    music

--- music.js
+++ music.js
@@ -1,3 +1,14 @@
+    function changeFrameHeight(){
+        var ifm= document.getElementById("iframepage"); 
+        ifm.height=document.documentElement.clientHeight;
+
+    }
+
+    window.onresize=function(){  
+         changeFrameHeight();  
+
+    } 
+
 $('.one-time').slick({
     dots: true,
     infinite: true,
