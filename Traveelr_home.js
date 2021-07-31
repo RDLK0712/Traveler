a506ac4c24707b8fa081791ab26330c0d2c2f8dc
heldho <1020313@ntjh.ntct.edu.tw>
2021-07-31 14:42:10 +0800

    首頁

---
+++ Traveelr_home.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        arrows: true,
+        slidesToShow: 1,
+        autoplay:true, 
+        autoplay:true,
+        autoplaySpeed:3000,
+    });
+});
